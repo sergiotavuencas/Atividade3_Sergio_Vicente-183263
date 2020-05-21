a25b2433078e6200029d7bdbd97b9109dd485fdb
sergiotavuencas <sergiovicente_tavuenc@outlook.com>
2020-05-21 01:12:27 -0300

    GenerationType.IDENTITY

--- atividade_03/src/main/java/com/atividade03/atividade_03/entity/Publisher.java
+++ atividade_03/src/main/java/com/atividade03/atividade_03/entity/Publisher.java
@@ -21,7 +21,7 @@ public class Publisher implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name", length = 80)
