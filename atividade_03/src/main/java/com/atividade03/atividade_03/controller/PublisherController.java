3ffb889bb8029f9610110169e3944990845e7a54
sergiotavuencas <sergiovicente_tavuenc@outlook.com>
2020-05-21 00:45:16 -0300

    @Validated, BindingResult e Detalhes

--- atividade_03/src/main/java/com/atividade03/atividade_03/controller/PublisherController.java
+++ atividade_03/src/main/java/com/atividade03/atividade_03/controller/PublisherController.java
@@ -1,12 +1,15 @@
 package com.atividade03.atividade_03.controller;
 
 import com.atividade03.atividade_03.entity.Publisher;
+import com.atividade03.atividade_03.service.BookService;
 import com.atividade03.atividade_03.service.PublisherService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,8 +21,11 @@ public class PublisherController {
     @Autowired
     private PublisherService publisherService;
 
+    @Autowired
+    private BookService bookService;
+
     @GetMapping("/template")
-    public ModelAndView getAuthors() {
+    public ModelAndView getTemplate() {
         ModelAndView mv = new ModelAndView("publisherTemplate");
         mv.addObject("publisher", new Publisher());
         mv.addObject("publishers", publisherService.getAllPublishers());
@@ -27,8 +33,20 @@ public class PublisherController {
     }
 
     @PostMapping("/register")
-    public String savePublisher (@ModelAttribute Publisher publisher) {
+    public String savePublisher (@Validated Publisher publisher, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "registerError";
+        }
         publisherService.createPublisher(publisher);
         return "redirect:/publisher/template";
+    }
+
+    @GetMapping("/details/{id}")
+    public ModelAndView getPublisher(@PathVariable(name = "id") Integer id) {
+        ModelAndView mv = new ModelAndView("publisherDetails");
+        Publisher publisher = publisherService.getPublisherById(id);
+        mv.addObject("publisher", publisher);
+        mv.addObject("books", bookService.getAllBooks());
+        return mv;
     }
 }
