94bed400f7de71c54f41ef2c4133352745c72ff8
sergiotavuencas <sergiovicente_tavuenc@outlook.com>
2020-05-21 18:50:57 -0300

    relacionar Livro Autor

--- atividade_03/src/main/java/com/atividade03/atividade_03/controller/AuthorController.java
+++ atividade_03/src/main/java/com/atividade03/atividade_03/controller/AuthorController.java
@@ -1,6 +1,9 @@
 package com.atividade03.atividade_03.controller;
 
+import java.util.List;
+
 import com.atividade03.atividade_03.entity.Author;
+import com.atividade03.atividade_03.entity.Book;
 import com.atividade03.atividade_03.service.AuthorService;
 import com.atividade03.atividade_03.service.BookService;
 
@@ -9,9 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -24,6 +29,7 @@ public class AuthorController {
     @Autowired
     private BookService bookService;
 
+    // Gets
     @GetMapping("/template")
     public ModelAndView getTemplate() {
         ModelAndView mv = new ModelAndView("authorTemplate");
@@ -32,21 +38,37 @@ public class AuthorController {
         return mv;
     }
 
+    @GetMapping("/details/{id}")
+    public ModelAndView getAuthor(@PathVariable(name = "id") Integer id) {
+        ModelAndView mv = new ModelAndView("authorDetails");
+        Author author = authorService.getAuthorById(id);
+        mv.addObject("author", author);
+
+        List<Book> booksNotLinked = bookService.getAllBooks();
+        booksNotLinked.removeAll(author.getBooks());
+        
+        mv.addObject("books", booksNotLinked);
+        return mv;
+    }
+
+    // Posts
     @PostMapping("/register")
     public String saveAuthor(@Validated Author author, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             return "registerError";
         }
-        authorService.createAuthor(author);
+        authorService.saveAuthor(author);
         return "redirect:/author/template";
     }
 
-    @GetMapping("/details/{id}")
-    public ModelAndView getBook(@PathVariable(name = "id") Integer id) {
-        ModelAndView mv = new ModelAndView("authorDetails");
-        Author author = authorService.getAuthorById(id);
-        mv.addObject("author", author);
-        mv.addObject("books", bookService.getAllBooks());
-        return mv;
+    @PostMapping("/linkBook")
+    public String linkBook(@ModelAttribute Book book, @RequestParam Integer author_id) {
+        Author author = authorService.getAuthorById(author_id);
+        book = bookService.getBookById(book.getId());
+
+        author.getBooks().add(book);
+        authorService.saveAuthor(author);
+
+        return "redirect:/author/details/" + author_id;
     }
 }
--- atividade_03/src/main/java/com/atividade03/atividade_03/controller/BookController.java
+++ atividade_03/src/main/java/com/atividade03/atividade_03/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.atividade03.atividade_03.controller;
 
+import java.util.List;
+
+import com.atividade03.atividade_03.entity.Author;
 import com.atividade03.atividade_03.entity.Book;
 import com.atividade03.atividade_03.service.AuthorService;
 import com.atividade03.atividade_03.service.BookService;
@@ -10,9 +13,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -28,6 +33,7 @@ public class BookController {
     @Autowired
     private PublisherService publisherService;
 
+    // Gets
     @GetMapping("/template")
     public ModelAndView getTemplate() {
         ModelAndView mv = new ModelAndView("bookTemplate");
@@ -37,22 +43,38 @@ public class BookController {
         mv.addObject("publishers", publisherService.getAllPublishers());
         return mv;
     }
-    
-    @PostMapping("/register")
-    public String saveBook(@Validated Book book, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()) {
-            return "registerError";
-        }
-        bookService.createBook(book);
-        return "redirect:/book/template";
-    }
 
     @GetMapping("/details/{id}")
     public ModelAndView getBook(@PathVariable(name = "id") Integer id) {
         ModelAndView mv = new ModelAndView("bookDetails");
         Book book = bookService.getBookById(id);
         mv.addObject("book", book);
-        mv.addObject("authors", authorService.getAllAuthors());
+
+        List<Author> authorsNotLinked = authorService.getAllAuthors();
+        authorsNotLinked.removeAll(book.getAuthors());
+        
+        mv.addObject("authors", authorsNotLinked);
         return mv;
+    }
+    
+    // Posts
+    @PostMapping("/register")
+    public String saveBook(@Validated Book book, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "registerError";
+        }
+        bookService.saveBook(book);
+        return "redirect:/book/template";
+    }
+
+    @PostMapping("/linkAuthor")
+    public String linkAuthor(@ModelAttribute Author author, @RequestParam Integer book_id) {
+        Book book = bookService.getBookById(book_id);
+        author = authorService.getAuthorById(author.getId());
+
+        book.getAuthors().add(author);
+        bookService.saveBook(book);
+
+        return "redirect:/book/details/" + book_id;
     }
 }
